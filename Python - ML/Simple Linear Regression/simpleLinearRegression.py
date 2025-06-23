970c432216ad340a42cb5c8e09a8495d0311c0bf
Robtrot <71306198+DanielSantana1234@users.noreply.github.com>
2025-06-23 16:49:41 -0400

    Easy Linear Regression

--- Python - ML/Simple Linear Regression/simpleLinearRegression.py
+++ Python - ML/Simple Linear Regression/simpleLinearRegression.py
@@ -1,11 +1,21 @@
+#Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Simple Linear Regression/SalaryData.csv')
+#Importing the dataset
+dataset = pd.read_csv('Python - ML/Simple Linear Regression/SalaryData.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+#Splitting the dataset into the training set and the test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+#Training the Simple Linear Regression model on the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression() 
+regressor.fit(X_train, y_train)
+
+#Predicting the Test set results
+y_pred = regressor.predict(X_test)
